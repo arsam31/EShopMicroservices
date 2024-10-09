36a4da9050a9921ef5b5c83440610314f26cc5a1
Hafiz <arsamrahmaan31@gmail.com>
2024-10-09 23:59:15 +0500

    registered and configured marten package

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container.
 builder.Services.AddCarter();
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+});
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
@@ -12,5 +16,4 @@ var app = builder.Build();
 
 // configure the HTTP request pipeline.
 app.MapCarter();
-
 app.Run();
